552a6c33060259b41aead10d45ae70d9c135fadf
James Cleveland <jamescleveland@gmail.com>
2012-01-23 03:05:56 +0000

    gettin there

--- subdown.py
+++ subdown.py
@@ -3,6 +3,76 @@ from twisted.web.client import getPage, downloadPage
 from twisted.internet.defer import DeferredList
 import json
 
+class Submission(object):
+    def __init__(self, child, subreddit):
+        self.data = child['data']
+        self.subreddit = self.data['subreddit']
+    
+    @property
+    def directory(self):
+        return self.subreddit
+
+    @property
+    def url(self):
+        return self.data['url']
+
+    @property
+    def file_name(self):
+        return self.data['url'].split('/')[-1]
+
+    @property
+    def ext(self):
+        return self.file_name.split('.')[-1]
+    
+    @property
+    def file_path(self):
+        return '%s/%s' % (self.directory, self.file_name)
+
+    def process(self):
+        """Returns a deferred(?) that will either:
+            1. Download an image directly.
+            2. Scrape the page of a link (ie a tumblr).
+            3. Does nothing because it's not the kind of link we want.
+        """
+        def changeFileDate(result):
+            """Changes the file date on a downloaded file."""
+            print "changing file date", self.file_path
+            # The date would be in child['data']['created'] but I'm not sure how to acc
+            # cess that here?
+
+        def checkFileSize(result):
+            """Fail on files lower than a set size"""
+            print "checking file size", self.file_path
+            if 1 == 1:
+                raise TooSmallError
+
+        def deleteFile(failure):
+            """Delete the resulting file."""
+            failure.trap(TooSmallError)
+            print "deleting file", self.file_path
+
+        def writeError(failure):
+            print "YO GOT TO WRIE ERR"
+            failure.trap(IOError)
+            print failure.getErrorMessage()
+
+        def finishChild(result):
+            print "Finished child", self.subreddit, self.file_path, self.url
+
+
+        if self.ext in ['jpg', 'png', 'gif']:
+            d = downloadPage(str(self.url), self.file_path)
+        else:
+            raise UnknownLinkError(self.url)
+
+        d.addCallback(checkFileSize)
+        d.addCallbacks(changeFileDate, deleteFile)
+        d.addErrback(writeError)
+        d.addBoth(finishChild)
+        print "trying to download", str(self.url), self.file_path
+
+        return d
+
 class UnknownLinkError(Exception):
     """This error is thrown when the program does not know what to do with a
     link.
@@ -18,49 +88,6 @@ def pageCallback(result):
   return len(result)
 
 
-def processSubmission(child, subreddit):
-    """Returns a deferred(?) that will either:
-        1. Download an image directly.
-        2. Scrape the page of a link (ie a tumblr).
-        3. Does nothing because it's not the kind of link we want.
-    """
-    data = child['data']
-    file_name = data['url'].split('/')[-1]
-    ext = file_name.split('.')[-1]
-    directory = subreddit
-    file_path = '%s/%s' % (directory, file_name)
-
-
-    if ext in ['jpg', 'png', 'gif']:
-        d = downloadPage(str(data['url']), file_path)
-    else:
-        raise UnknownLinkError(data['url'])
-
-    print "trying to download", str(data['url']), file_path
-
-    return d
-
-def changeFileDate(result, subreddit):
-    """Changes the file date on a downloaded file."""
-    print "changing file date", result, subreddit
-    # The date would be in child['data']['created'] but I'm not sure how to acc
-    # cess that here?
-
-def checkFileSize(result, subreddit):
-    """Fail on files lower than a set size"""
-    print "checking file size", result, subreddit
-
-def deleteFile(failure):
-    """Delete the resulting file."""
-    print "HI GOT TO DELETE YAEH"
-    failure.trap(TooSmallError)
-    print "deleting file", failure.getErrorMessage()
-
-def writeError(failure):
-    print "YO GOT TO WRIE ERR"
-    failure.trap(IOError)
-    print failure.getErrorMessage()
-
 def subredditCallback(page, subreddit, max_count, count=1):
     """Load x many pages of a subreddit and then do ??"""
     data = json.loads(page)['data']
@@ -71,10 +98,8 @@ def subredditCallback(page, subreddit, max_count, count=1):
     if data['children']:
         for child in data['children']:
             try:
-                d = processSubmission(child, subreddit)
-                d.addCallback(checkFileSize)
-                d.addCallbacks(changeFileDate, deleteFile)
-                d.addErrback(writeError)
+                submission = Submission(child, subreddit)
+                d = submission.process()
                 dlist.append(d)
             except UnknownLinkError:
                 print "Didn't know how to handle link.", child['data']['url']
@@ -104,7 +129,7 @@ def main():
     max_count = 1
                 
     subreddits = [
-        'pics'
+        'wallpapeRs'
     ]
 
     dlist = []
