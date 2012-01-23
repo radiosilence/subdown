7261bb9db01bfec9437c49846bf5a62866d90ff8
James Cleveland <jamescleveland@gmail.com>
2012-01-23 02:43:39 +0000

    more barely working code

--- subdown.py
+++ subdown.py
@@ -1,89 +1,121 @@
 from twisted.internet import reactor
 from twisted.web.client import getPage, downloadPage
 from twisted.internet.defer import DeferredList
-
 import json
+
+class UnknownLinkError(Exception):
+    """This error is thrown when the program does not know what to do with a
+    link.
+    """
+    pass
+
+
+class TooSmallError(Exception):
+    pass
 
 
 def pageCallback(result):
   return len(result)
 
-def processSubmission(child):
+
+def processSubmission(child, subreddit):
     """Returns a deferred(?) that will either:
         1. Download an image directly.
         2. Scrape the page of a link (ie a tumblr).
         3. Does nothing because it's not the kind of link we want.
     """
-    
-    subreddit_name = "BLAH" #  Don't know how to pass this in!!
-    file_name = child['data']['url'].split('/')[-1]
-    d = downloadPage(child['data']['url'], '%s/%s'
-        % (subreddit_name, file_name))
+    data = child['data']
+    file_name = data['url'].split('/')[-1]
+    ext = file_name.split('.')[-1]
+    directory = subreddit
+    file_path = '%s/%s' % (directory, file_name)
 
-    return the_deferred
 
-def changeFileDate(result):
+    if ext in ['jpg', 'png', 'gif']:
+        d = downloadPage(str(data['url']), file_path)
+    else:
+        raise UnknownLinkError(data['url'])
+
+    print "trying to download", str(data['url']), file_path
+
+    return d
+
+def changeFileDate(result, subreddit):
     """Changes the file date on a downloaded file."""
-
+    print "changing file date", result, subreddit
     # The date would be in child['data']['created'] but I'm not sure how to acc
     # cess that here?
-    pass
 
-def checkFileSize(result):
+def checkFileSize(result, subreddit):
     """Fail on files lower than a set size"""
-    pass
+    print "checking file size", result, subreddit
 
-def deleteFile(result):
+def deleteFile(failure):
     """Delete the resulting file."""
-    pass
+    print "HI GOT TO DELETE YAEH"
+    failure.trap(TooSmallError)
+    print "deleting file", failure.getErrorMessage()
 
-def subredditCallback(result):
+def writeError(failure):
+    print "YO GOT TO WRIE ERR"
+    failure.trap(IOError)
+    print failure.getErrorMessage()
+
+def subredditCallback(page, subreddit, max_count, count=1):
     """Load x many pages of a subreddit and then do ??"""
-    data = json.loads(result)
+    data = json.loads(page)['data']
 
-    count = 2  # no idea how get this from here?? needs to be next page count
     previous_after = 'string'  # no idea how to get this either
 
     dlist = []
     if data['children']:
         for child in data['children']:
-            d = processSubmission(child)
-            d.addCallback(checkFileSize)
-            d.addBoth(changeFileDate, deleteFile))
-            dlist.append(d)
+            try:
+                d = processSubmission(child, subreddit)
+                d.addCallback(checkFileSize)
+                d.addCallbacks(changeFileDate, deleteFile)
+                d.addErrback(writeError)
+                dlist.append(d)
+            except UnknownLinkError:
+                print "Didn't know how to handle link.", child['data']['url']
 
     # This is the stuff for getting the next page. I guess we do it recursivel
     # y rather than iteratively because async?
+    print subreddit, data['after'], previous_after, count, max_count
     if data['after'] != previous_after and count < max_count:
-        d = getPage('http://www.reddit.com/r/wallpapers/.json?count=%s&after=%s'
-            % (count, data['after']))
-        d.addCallback(subredditCallback)
+        new_count = count + 1
+        print "getting page", str('http://www.reddit.com/r/%s/.json?count=%s&after=%s'
+            % (subreddit, new_count, data['after']))
+
+        d = getPage(str('http://www.reddit.com/r/%s/.json?count=%s&after=%s'
+            % (subreddit, new_count, data['after'])))
+        d.addCallback(subredditCallback, subreddit, max_count, new_count)
         dlist.append(d)
     
-    return deferredList(dlist)
+    return DeferredList(dlist)
 
 def finish(ign):
-  reactor.stop()
+    print "Reached the finish!"
+    reactor.stop()
 
 def main():
     # only get 5 pages - I need to pass this into the scope of subredditCallbac
     # k somehow, how should I do this?
-    max_count = 5
+    max_count = 1
                 
     subreddits = [
-        'wallpapers',
         'pics'
     ]
 
     dlist = []
     for subreddit in subreddits:
         d = getPage('http://www.reddit.com/r/%s/.json' % subreddit)
-        d.addCallback(subredditCallback)
+        d.addCallback(subredditCallback, subreddit, max_count)
         dlist.append(d)
     
-    d = deferredList(dlist)
+    d = DeferredList(dlist)
     d.addCallback(finish)
 
-if __name__ == '__main__';
+if __name__ == '__main__':
     main()
     reactor.run()
