1c46096459d6b658236ea89d37dea5a58f41b00c
James Cleveland <jc@blackflags.co.uk>
2012-02-06 01:54:08 +0000

    setup things

---
+++ setup.py
@@ -0,0 +1,24 @@
+"""
+subdown.py
+----------
+
+subdown.py is a tool for downloading images from reddit.
+"""
+
+from setuptools import setup, find_packages
+
+setup(
+    name='suave',
+    version='.1',
+    author='James Cleveland',
+    author_email='jc@blackflags.co.uk',
+    packages=find_packages(),
+    url='http://pypi.python.org/pypi/suave/',
+    license='LICENSE.txt',
+    description='A tool for downloading images from reddit.',
+    long_description=open('README.rst').read(),
+    install_requires=open('requirements.txt').read().split("\n"),
+    package_data={
+        '': ['*.txt', '*.rst']
+    }
+)
