30e0024c545b63060e1ec95a36b67ecd79f5e1ba
James Cleveland <jamescleveland@gmail.com>
2012-09-27 03:51:54 +0100

    cleaned up output

--- subdown.py
+++ subdown.py
@@ -19,7 +19,6 @@ TEMPLATE = 'http://www.reddit.com/r/{}/.json?count={}&after={}'
 
 Submission = namedtuple('Submission', 'url filename created encoding')
 
-#url = 'http://localhost:{}'
 
 def url_filename(url):
     def useful_part(url):
@@ -31,11 +30,12 @@ def url_filename(url):
     else:
         return useful_part(url)
 
-def get_page(subreddit, page, after):
-    url = TEMPLATE.format(subreddit, page, after)
+
+def get_page(subreddit, count, after, max_count):
+    url = TEMPLATE.format(subreddit, count, after)
     result = requests.get(url, timeout=2)
-    puts('Getting page')
-    puts(url)
+    puts('{} {} (Page {} of {})'.format(colored.green('==>'), subreddit,
+        count + 1, max_count))
     try:
         if result.status_code != 200:
             raise Exception
@@ -44,42 +44,46 @@ def get_page(subreddit, page, after):
         raise Exception('404 Not Found')
     return data['children'], result.encoding, data['after']
 
+
 def get_subreddit(subreddit, max_count, count=0, after=None):
     while count < max_count:
-        quote = colored.yellow('[{}/{}] '.format(count + 1, max_count))
-        with indent(len(quote), quote=quote):
-            children, encoding, after = get_page(subreddit, count, after)
-            download_children(children, encoding)
+        children, encoding, after = get_page(subreddit, count, after,
+            max_count)
+        download_children(children, encoding)
         count += 1
+
 
 def download_children(children, encoding):
     def valid(child):
         exts = ('jpg', 'jpeg', 'png', 'gif')
         return url_filename(child['data']['url']).split('.')[-1] in exts
 
-    puts("Downloading children")
     jobs = []
-    for child in filter(valid, children):
-        url = child['data']['url']
-        filename = url_filename(url)
-        submission = Submission(
-            url,
-            filename,
-            datetime.datetime.fromtimestamp(child['data']['created']),
-            encoding
-        )
-        puts(u'Added {}'.format(filename).encode(encoding))
-        jobs.append(gevent.spawn(download_submission, submission))
+    quote = '  {} '.format(colored.blue('->'))
+    with indent(len(quote), quote=quote):
+        for child in filter(valid, children):
+            url = child['data']['url']
+            filename = url_filename(url)
+            submission = Submission(
+                url,
+                filename,
+                datetime.datetime.fromtimestamp(child['data']['created']),
+                encoding
+            )
+            jobs.append(gevent.spawn(download_submission, submission))
 
-    gevent.joinall(jobs, timeout=10)
-    for job in jobs:
-        if not job.value:
-            puts(colored.red('Timed out {}'.format(
-                job.args[0].filename.encode(encoding))))
+        gevent.joinall(jobs, timeout=2)
+        for job in jobs:
+            if not job.value:
+                puts(colored.red('Timed out {}'.format(
+                    job.args[0].filename.encode(encoding))))
+                job.kill()
 
 def download_submission(submission):
     gevent.sleep(0.01*ord(os.urandom(1)))
-    puts(colored.green("DOWNLOADED ;) %s" % submission.filename.encode(submission.encoding)))
+    puts('Downloaded {}'.format(
+        submission.filename.encode(submission.encoding)
+    ))
     return True
 
 
@@ -97,13 +101,11 @@ if __name__ == '__main__':
         except:
             puts(colored.red('Failed to load subreddit {}'.format(subreddit)))
             continue
-        quote = ' -> {} '.format(subreddit)
-        with indent(len(quote), quote=quote):
-            try:
-                get_subreddit(subreddit, max_count)
-            except Exception as e:
-                raise
-                puts(colored.red(str(e)))
+        try:
+            get_subreddit(subreddit, max_count)
+        except Exception as e:
+            raise
+            puts(colored.red(str(e)))
 
 # urls = [url.format(port) for port in [8051, 8052]]
 # jobs = [gevent.spawn(requests.get, url) for url in urls]
