aa8759c2f27a26d6e28e9020b3db30c22b08977d
James Cleveland <jc@blackflags.co.uk>
2013-10-06 13:35:30 +0100

    Most functioanlity ported. TODO: Make it actually work.

--- subdown.ts
+++ subdown.ts
@@ -45,23 +45,24 @@ class Subreddit {
                     str += chunk;
                 });
                 response.on('end', function() {
-                    log.info(this.name, ["Got page", page].join(' '));
+                    log.info(this.name, "Got page", self.currentPage);
                     var data = JSON.parse(str).data,
                         submissions = data.submissions;
                     this.currentAfter = data.after;
                     this.currentPage += 1;
+                    this.name = data.subreddit;
                     resolve(submissions);
                 });
             })
         });
     }
-    processSubmissions(submissions: {}[]): Promise {
+    processSubmissions(submissions: Submission[]): Promise {
         return Promise.map(submissions, this.processSubmission);
     }
-    processSubmission(submission: {}): Promise  {
+    processSubmission(submission: Submission): Promise  {
         new Promise(function(resolve, reject) {
             var imageSet: ImageSet = new ImageSet(
-                Date(submission.data.created),
+                new Date(submission.data.created),
                 this
             );
             if (submission.data.url.match(/^http:\/\/i\.imgur\.com\/\w+\.(jpg|png|gif)$/)) {
@@ -71,33 +72,38 @@ class Subreddit {
         });
     }
     downloadImageSets(imageSets: ImageSet[]): Promise {
-        for (i in imageSets) {
+        for (var i in imageSets) {
             imageSets[i].download()
         }
+    }
+}
+
+interface Submission {
+    data: {
+        url: string;
+        created: number;
     }
 }
 
 class ImageSet {
     created: Date;
     subreddit: Subreddit;
-    images: Image[];
+    images: ImageURL[];
     constructor(created: Date, subreddit: Subreddit) {
         this.created = created;
         this.subreddit = subreddit;
     }
     addUrl(url: string) {
-        this.images.push(Image(url, this));
+        this.images.push(new ImageURL(url, this));
     }
     download(): Promise {
-        for (i in self.images) {
-            self.images[i].download();
+        for (var i in this.images) {
+            this.images[i].download();
         }
     }
-    
-    
 }
 
-class Image {
+class ImageURL {
     url: string;
     filename: string;
     path: string;
@@ -106,34 +112,56 @@ class Image {
     constructor(url: string, imageSet: ImageSet) {
         this.url = url;
         this.imageSet = imageSet;
-        this.path = this.url.split("/").slice(-1)[0];
-        this.filename = [this.imageSet.subreddit.name, this.filename()].join('/');
+        this.filename = this.url.split("/").slice(-1)[0];
+        this.path = [this.imageSet.subreddit.name, this.filename].join('/');
     }
-    download(): {
-        this.testFile(url).then(
-            this.save()
+    download() {
+        this.testFile(this.url).then(
+            this.save
         ).then(
             this.setUpdatedTime
-        ).catch(ImageAlreadyExistsError, function (){}
+        ).then(function() {
+            log.info(this.imageSet.subreddit.name, "Downloaded", this.url); 
+        }).catch(ImageAlreadyExistsError, function (){}
         ).catch(function(err) {
-            log.error(this.subreddit, url, err);
+            log.error(this.subreddit, this.url, err);
         });
     }
-    testFile(image: string) {
+    testFile(image: string): Promise {
         return fs.existsAsync(this.path).catch(function() {
             throw new ImageAlreadyExistsError();
         }).then(function() {
-            fs.mkdirAsync(subreddit).catch(function() {});
-        }).then(function() {
-            return [url, path];
+            fs.mkdirAsync(this.imageSet.subreddit.name).catch(function() {});
         });
     }
-}
-function main(names: string[], maxPages: number) {
-    for (i in names) {
-        s = Subreddit(names[i], maxPages);
-        s.get();
+    save(): Promise {
+        var parts: string[] = this.url.split('/'),
+            options = {
+                'host': parts[2],
+                'path': "/" + parts.slice(3).join('/')
+            };
+        return fs.openAsync(this.path, 'w').then(function (fd) {
+            http.request(options, function(res) {
+                res.on('error', function(err) {
+                    throw new Error(err);
+                }); 
+                res.on('data', function(chunk) {
+                    fs.writeAsync(fd, chunk, 0, chunk.length)
+                });
+                res.on('end', function() {
+                    fs.closeAsync(fd);
+                })
+            }).end()
+        });
+    }
+    setUpdatedTime(): void {
     }
 }
+
+function main(names: string[], maxPages: number) {
+    for (var i in names) {
+        new Subreddit(names[i], maxPages).get();
+    }
+}
+
 main(['dogpictures', 'labrador'], 10);
-// { "kind": string; "data": { "modhash": string; "children": {}[]; "after": string; "before": null; }; }
